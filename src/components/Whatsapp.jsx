5b917886fcd01cb007f0a2ae8b6b5a30a3583d70
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-31 23:50:45 +0530

    add whatsapp button

---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,12 @@
+import './Whatsapp.css';
+import icon from '../assets/icons/whatsapp.svg';
+
+function Whatsapp() {
+    return (
+        <div className="Whatsapp">
+            <img src={icon} alt="whatsapp" />
+        </div>
+    );
+}
+
+export default Whatsapp;
