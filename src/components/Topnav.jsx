7ba715f4fdea07aed47c3c3c0b08ed6a1eae15d7
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-31 23:53:08 +0530

    add icons

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -5,13 +5,28 @@ function Topnav() {
         <div className="Topnav">
             <div className="topnav__container">
                 <div className="topnav__left">
-                    <a className="topnav__link" href="tel:+918726127335">+91 87261 27335</a>
-                    <a className="topnav__link" href="mailto:support@vansystems.co.in">support@vansystems.co.in</a>
+                    <a className="topnav__link" href="tel:+918726127335">
+                        <span className="material-symbols-rounded">call</span>
+                        +91 87261 27335
+                    </a>
+                    <a className="topnav__link" href="mailto:support@vansystems.co.in">
+                        <span className="material-symbols-rounded">mail</span>
+                        support@vansystems.co.in
+                    </a>
                 </div>
                 <div className="topnav__right">
-                    <a className="topnav__link" href="#">Help</a>
-                    <a className="topnav__link" href="#">Register</a>
-                    <a className="topnav__link" href="#">Login</a>
+                    <a className="topnav__link" href="#">
+                        <span className="material-symbols-rounded">help</span>
+                        Help
+                    </a>
+                    <a className="topnav__link" href="#">
+                        <span className="material-symbols-rounded">person</span>
+                        Register
+                    </a>
+                    <a className="topnav__link" href="#">
+                        <span className="material-symbols-rounded">lock</span>
+                        Login
+                    </a>
                 </div>
             </div>
         </div>
