fa8f4ba72f74d769080c1f7111e58b605c847b51
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-31 23:50:24 +0530

    add scroll to top button

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,11 @@
+import './Scroll.css';
+
+function Scroll() {
+    return (
+        <div className="Scroll">
+            <span className="material-symbols-rounded">expand_less</span>
+        </div>
+    );
+}
+
+export default Scroll;
